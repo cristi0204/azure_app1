c2eabafe8d154bf59420cfd3006b126de2a99c8d
cristi0204 <67113543+cristi0204@users.noreply.github.com>
2023-01-14 15:03:39 +0200

    app change

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # this is the main app to load
-
+""""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,3 +7,69 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+"""
+
+import redis, sqlite3, time
+from flask import Flask, render_template, request, g, current_app
+
+app = Flask(__name__)
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+conn = sqlite3.connect('trade.db')
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS signals (
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
+        ticker,
+        order_action,
+        order_contracts,
+        order_price
+    )
+""")
+conn.commit()
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect('trade.db')
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+@app.get('/')
+def dashboard():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT * FROM signals
+    """)
+    signals = cursor.fetchall()
+
+    return render_template('dashboard.html', signals=signals)
+
+@app.post("/webhook")
+def webhook():
+    data = request.data
+
+    if data:
+        r.publish('tradingview', data)
+
+        data_dict = request.json
+
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("""
+            INSERT INTO signals (ticker, order_action, order_contracts, order_price) 
+            VALUES (?, ?, ?, ?)
+        """, (data_dict['ticker'], 
+                data_dict['strategy']['order_action'], 
+                data_dict['strategy']['order_contracts'],
+                data_dict['strategy']['order_price']))
+
+        db.commit()
+
+        return data
+
+    return {
+        "code": "success"
+    }
